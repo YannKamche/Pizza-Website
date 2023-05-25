bab7609ca27f9ae1cf0aacd98a21a188afbac909
Yann Kamche <yannkamche@gmail.com>
2023-05-25 09:07:39 +0100

    Added the script file

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*~~~~~~~~~~~~~~~ TOGGLE MENU ~~~~~~~~~~~~~~~*/
+/* MENU SHOW */
+
+
+/* MENU HIDDEN */
+
+/* REMOVE MENU MOBILE */
+
+/*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ DARK LIGHT THEME ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
