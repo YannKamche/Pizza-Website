8d3193e78e44c416b9e6ac1b2235a54f3213a929
Yann Kamche <yannkamche@gmail.com>
2023-05-26 15:32:47 +0100

    Added menu toggle function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+const navLink = document.querySelectorAll(".nav__link")
 /*~~~~~~~~~~~~~~~ TOGGLE MENU ~~~~~~~~~~~~~~~*/
 /* MENU SHOW */
-
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
 /* MENU HIDDEN */
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove("show-menu")
+    })
+}
 
 /* REMOVE MENU MOBILE */
-
+navLink.forEach(link => link.addEventListener('click', () => {
+    navMenu.classList.remove("show-menu")
+}))
 /*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
 
 /*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
